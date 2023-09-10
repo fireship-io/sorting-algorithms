b137efa8efbbe04e2fcf424965c93a389df57712
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2023-09-10 02:10:06 -0400

    modular no-temp swap

--- src/bubble.js
+++ src/bubble.js
@@ -1,10 +1,15 @@
+/**
+Swaps 2 indices within array (in-place).
+*/
+const swap = (arr, i, j) => {
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+};
+
 export function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+        swap(arr, j, j + 1);
       }
     }
   }
